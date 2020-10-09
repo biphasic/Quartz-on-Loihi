9bddc87124d02bb311e64e07b8c7debec018101d
Gregor Lenz <mail@lenzgregor.com>
2020-10-09 07:22:19 -0700

    prevent small error in convpool block

--- quartz/blocks.py
+++ quartz/blocks.py
@@ -194,7 +194,7 @@ class ConvMax(Block):
         self.neurons += conv_neurons
 
         weight_e, weight_acc, t_min, t_neu = self.get_params_at_once()
-        first.connect_to(first, -weight_e)
+        first.connect_to(first, -1.8*weight_e)
         second.connect_to(second, -weight_acc)
         second.connect_to(first, weight_e)
         for neuron in conv_neurons:
