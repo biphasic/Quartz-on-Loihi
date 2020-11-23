39da8e2f48caa5d3edb1e2d196b0d6320bb03e59
Gregor Lenz <mail@lenzgregor.com>
2020-11-23 19:09:36 +0100

    73% cifar10

--- CNN/cifar_model.py
+++ CNN/cifar_model.py
@@ -7,26 +7,34 @@ import ipdb
 class ConvNet(nn.Module):
     def __init__(self, n_classes):
         super(ConvNet, self).__init__()
-        drop = 0.05
+        drop = 0.3
         conv_bias = True
-        l1_n_channels = 6
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=l1_n_channels, kernel_size=5, stride=1, bias=conv_bias)
+        l1_n_channels = 32
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=l1_n_channels, kernel_size=3, stride=1, bias=conv_bias)
         self.pool1 = nn.MaxPool2d(kernel_size=2)
         self.drop1 = nn.Dropout2d(0.5*drop)
         self.bn1 = nn.BatchNorm2d(l1_n_channels)
         
-        l2_n_channels = 12
-        self.conv2 = nn.Conv2d(in_channels=l1_n_channels, out_channels=l2_n_channels, kernel_size=5, stride=1, bias=conv_bias)
+        l2_n_channels = 64
+        self.conv2 = nn.Conv2d(in_channels=l1_n_channels, out_channels=l2_n_channels, kernel_size=4, stride=1, bias=conv_bias)
         self.pool2 = nn.MaxPool2d(kernel_size=2)
         self.drop2 = nn.Dropout2d(drop)
         self.bn2 = nn.BatchNorm2d(l2_n_channels)
+        
+#         l3_n_channels = 64
+#         self.conv3 = nn.Conv2d(in_channels=l1_n_channels, out_channels=l2_n_channels, kernel_size=3, stride=1, bias=conv_bias)
+#         self.pool3 = nn.MaxPool2d(kernel_size=2)
+#         self.drop3 = nn.Dropout2d(drop)
+#         self.bn2 = nn.BatchNorm2d(l2_n_channels)
 
-        l3_n_channels = 120
-        self.conv3 = nn.Conv2d(in_channels=l2_n_channels, out_channels=l3_n_channels, kernel_size=4, stride=1, bias=conv_bias)
+        l3_n_channels = 200
+        self.conv3 = nn.Conv2d(in_channels=l2_n_channels, out_channels=l3_n_channels, kernel_size=6, stride=1, bias=conv_bias)
         self.drop3 = nn.Dropout2d(drop)
         self.bn3 = nn.BatchNorm2d(l3_n_channels)
-
-        self.fc1 = nn.Linear(in_features=l3_n_channels, out_features=n_classes)
+        
+        l4_n_channels = 100
+        self.fc1 = nn.Linear(in_features=l3_n_channels, out_features=l4_n_channels)
+        self.fc2 = nn.Linear(in_features=l4_n_channels, out_features=n_classes)
 
 
     def forward(self, x):
@@ -48,6 +56,9 @@ class ConvNet(nn.Module):
         l3_out = self.drop3(l3_out)
         l3_out = torch.flatten(l3_out, 1)
 
-        logits = self.fc1(l3_out)
+        l4_out = self.fc1(l3_out)
+        l4_out = F.relu(l4_out)
+        
+        logits = self.fc2(l4_out)
         probs = F.softmax(logits, dim=1)
         return logits, probs, l1_out, l2_out, l3_out
