a5f65c767c7dc45ca4d34141b8564918ce41c923
Gregor Lenz <mail@lenzgregor.com>
2020-12-04 15:09:40 +0100

    achieving ~80% using mobilenetv2 bottleneck layers

--- CNN/cifar_model.py
+++ CNN/cifar_model.py
@@ -4,83 +4,18 @@ from torch.nn import functional as F
 import ipdb
 
 
-class depthwise_separable_conv(nn.Module):
-    def __init__(self, nin, nout):
-        super(depthwise_separable_conv, self).__init__()
-        self.depthwise = nn.Conv2d(nin, nin, kernel_size=3, padding=1, groups=nin)
-        self.pointwise = nn.Conv2d(nin, nout, kernel_size=1)
-
-    def forward(self, x):
-        out = self.depthwise(x)
-        out = self.pointwise(out)
-        return out
-
-    
-class ConvBNReLU(nn.Sequential):
-    def __init__(self, in_planes: int, out_planes: int, kernel_size: int = 3, stride: int = 1, groups: int = 1):
-        padding = (kernel_size - 1) // 2
-        super(ConvBNReLU, self).__init__(
-            nn.Conv2d(in_planes, out_planes, kernel_size, stride, padding, groups=groups, bias=False),
-            nn.BatchNorm2d(out_planes),
-            nn.ReLU6(inplace=True)
-        )
-
-
-class InvertedResidual(nn.Module):
-    def __init__(self, expansion_factor, output_channels, repeats, stride):
-        super(InvertedResidual, self).__init__()
-        self.stride = stride
-
-        hidden_dim = int(round(inp * expand_ratio))
-        self.use_res_connect = self.stride == 1 and inp == oup
-
-        layers: List[nn.Module] = []
-        if expand_ratio != 1:
-            # pw
-            layers.append(ConvBNReLU(inp, hidden_dim, kernel_size=1, norm_layer=norm_layer))
-        layers.extend([
-            # dw
-            ConvBNReLU(hidden_dim, hidden_dim, stride=stride, groups=hidden_dim, norm_layer=norm_layer),
-            # pw-linear
-            nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
-            norm_layer(oup),
-        ])
-        self.conv = nn.Sequential(*layers)
-
-
-class ConvPool(nn.Module):
-    drop_conv = 0.4
-    conv_bias = False
-    def __init__(self, in_channels, out_channels, kernel_size, stride=1):
-        super(ConvPool, self).__init__()
-        self.conv_pool = nn.Sequential(
-            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, 
-                      kernel_size=kernel_size, stride=stride, bias=self.conv_bias),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2),
-            nn.Dropout2d(self.drop_conv),
-            nn.BatchNorm2d(out_channels),
-        )
-    def forward(self, x):
-        return self.conv_pool(x)
-
-        
 class ConvNet(nn.Module):
     def __init__(self, n_classes):
-        super(ConvNet, self).__init__()
-        drop_dense = 0.4
-        
-        features = []
-        features.append(ConvPool(in_channels=3, out_channels=32, kernel_size=3, stride=1))
-        features.append(ConvPool(in_channels=32, out_channels=64, kernel_size=4, stride=1))
-        self.features = nn.Sequential(*features)
-        
+        super(ConvNet, self).__init__()        
+        self.features = nn.Sequential(
+            ConvBNReLU(in_channels=3, out_channels=32, kernel_size=3, stride=2),
+            Bottleneck(in_channels=32, out_channels=64, expansion_factor=4, stride=2),
+            Bottleneck(in_channels=64, out_channels=128, expansion_factor=4, stride=2),
+        )
         self.classifier = nn.Sequential(
-            nn.Conv2d(in_channels=64, out_channels=200, kernel_size=6, stride=1, bias=True),
-            nn.ReLU(),
-            nn.BatchNorm2d(200),
+            ConvPool(in_channels=128, out_channels=256, conv_kernel_size=1, pool_kernel_size=4, stride=1),
             nn.Flatten(),
-            nn.Linear(in_features=200, out_features=n_classes),
+            nn.Linear(in_features=256, out_features=n_classes),
         )
 
     def forward(self, out):
@@ -89,15 +24,60 @@ class ConvNet(nn.Module):
         probs = F.softmax(logits, dim=1)
         return logits, probs
 
-    
-class Logger:
-    def __init__(self):
-        self.outputs = []
-        self.n_layers = 0
-        
-    def __call__(self, module, module_in, module_out):
-        self.outputs.append(module_out)
-        
-    def clear(self):
-        del(self.outputs)
-        self.outputs = []
+
+class ConvBNReLU(nn.Sequential):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, groups=1):
+        padding = (kernel_size - 1) // 2
+        super(ConvBNReLU, self).__init__(
+            nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, groups=groups, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU6(inplace=True)
+        )
+
+
+class Bottleneck(nn.Module):
+    def __init__(self, in_channels, out_channels, expansion_factor, repeats=1, stride=1):
+        super(Bottleneck, self).__init__()
+        hidden_dim = int(round(in_channels * expansion_factor))
+        self.use_res_connect = stride == 1 and in_channels == out_channels
+
+        layers = []
+        if expansion_factor != 1:
+            layers += ConvBNReLU(in_channels, hidden_dim, kernel_size=1, stride=1)
+        layers += ConvBNReLU(hidden_dim, hidden_dim, kernel_size=3, stride=stride, groups=hidden_dim),
+        # even though the mobilenetv2 paper advocates against the use of ReLU for this layer, our SNN only supports ReLU output
+        layers += ConvBNReLU(hidden_dim, out_channels, kernel_size=1, stride=1)        
+        self.conv = nn.Sequential(*layers)
+
+    def forward(self, x):
+        if self.use_res_connect:
+            return x + self.conv(x)
+        else:
+            return self.conv(x)
+
+
+class ConvPool(nn.Sequential):
+    drop_conv = 0.2
+    conv_bias = False
+    def __init__(self, in_channels, out_channels, conv_kernel_size, pool_kernel_size=2, stride=1):
+        super(ConvPool, self).__init__(
+            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, 
+                      kernel_size=conv_kernel_size, stride=stride, bias=self.conv_bias),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=pool_kernel_size),
+            nn.Dropout2d(self.drop_conv),
+            nn.BatchNorm2d(out_channels),
+        )
+
+
+class DepthwiseSeparableConv(nn.Sequential):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1):
+        padding = (kernel_size - 1) // 2
+        super(depthwise_separable_conv, self).__init__(
+            nn.Conv2d(in_channels, in_channels, kernel_size, stride, padding, groups=in_channels),
+            nn.BatchNorm2d(in_channels),
+            nn.ReLU(),
+            nn.Conv2d(in_channels, out_channels, kernel_size=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(),
+        )
