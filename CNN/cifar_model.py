a94bd4fdba1ac94b9f1879d8e50e9bcace6d80d2
Gregor Lenz <mail@lenzgregor.com>
2021-05-04 15:50:28 +0200

    update cifar training nb and utils

--- CNN/cifar_model.py
+++ CNN/cifar_model.py
@@ -4,19 +4,19 @@ from torch.nn import functional as F
 import ipdb
 
 
-class ConvNet(nn.Module):
+class MobileNet(nn.Module):
     def __init__(self, n_classes):
-        super(ConvNet, self).__init__()        
+        super(MobileNet, self).__init__()        
         self.features = nn.Sequential(
             ConvBNReLU(in_channels=3, out_channels=32, kernel_size=3, stride=2),
             Bottleneck(in_channels=32, out_channels=64, expansion_factor=4, stride=2),
-            #Bottleneck(in_channels=64, out_channels=64, expansion_factor=4, stride=1),
+            Bottleneck(in_channels=64, out_channels=64, expansion_factor=4, stride=1),
             Bottleneck(in_channels=64, out_channels=128, expansion_factor=4, stride=2),
         )
         self.classifier = nn.Sequential(
-            ConvPool(in_channels=128, out_channels=128, conv_kernel_size=1, pool_kernel_size=4, stride=1),
+            ConvPool(in_channels=128, out_channels=160, conv_kernel_size=1, pool_kernel_size=4, stride=1),
             nn.Flatten(),
-            nn.Linear(in_features=128, out_features=n_classes),
+            nn.Linear(in_features=160, out_features=n_classes),
         )
 
     def forward(self, out):
@@ -32,29 +32,33 @@ class ConvBNReLU(nn.Sequential):
         super(ConvBNReLU, self).__init__(
             nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, groups=groups, bias=False),
             nn.BatchNorm2d(out_channels, momentum=0.4),
-            nn.ReLU6(inplace=True)
-        )
+            nn.ReLU6(inplace=True))
 
+# def ConvBNReLU(self, in_channels, out_channels, kernel_size=3, stride=1, groups=1):
+#     padding = (kernel_size - 1) // 2
+#     return [nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, groups=groups, bias=False),
+#             nn.BatchNorm2d(out_channels, momentum=0.4),
+#             nn.ReLU6(inplace=True)]
 
 class Bottleneck(nn.Module):
     def __init__(self, in_channels, out_channels, expansion_factor, repeats=1, stride=1):
         super(Bottleneck, self).__init__()
         hidden_dim = int(round(in_channels * expansion_factor))
-        self.use_res_connect = stride == 1 and in_channels == out_channels
 
         layers = []
         if expansion_factor != 1:
             layers += ConvBNReLU(in_channels, hidden_dim, kernel_size=1, stride=1)
         layers += ConvBNReLU(hidden_dim, hidden_dim, kernel_size=3, stride=stride, groups=hidden_dim),
-        # even though the mobilenetv2 paper advocates against the use of ReLU for this layer, our SNN only supports ReLU output
-        layers += ConvBNReLU(hidden_dim, out_channels, kernel_size=1, stride=1)        
-        self.conv = nn.Sequential(*layers)
+        # add bottleneck
+        layers += nn.Sequential(
+            nn.Conv2d(hidden_dim, out_channels, kernel_size=1, stride=1, bias=False),
+            nn.BatchNorm2d(out_channels, momentum=0.4),
+            #nn.ReLU6(),
+        )
+        self.bottleneck = nn.Sequential(*layers)
 
     def forward(self, x):
-        if self.use_res_connect:
-            return x + self.conv(x)
-        else:
-            return self.conv(x)
+        return self.bottleneck(x)
 
 
 class ConvPool(nn.Sequential):
--- CNN/utils.py
+++ CNN/utils.py
@@ -27,6 +27,22 @@ def get_accuracy(model, data_loader, device):
             correct_pred += (predicted_labels == y_true).sum()
     return correct_pred.float() / n
 
+def get_folded_accuracy(model, data_loader, device):
+    correct_pred = 0 
+    n = 0
+    with torch.no_grad():
+        model.eval()
+        for X, y_true in data_loader:
+            X = X.to(device)
+            y_true = y_true.to(device)
+            y_output = model(X)
+            y_prob = F.softmax(y_output, dim=1)
+            _, predicted_labels = torch.max(y_prob, 1)
+            n += y_true.size(0)
+            correct_pred += (predicted_labels == y_true).sum()
+    return correct_pred.float() / n
+    
+    
 def plot_losses(train_losses, valid_losses):
     # temporarily change the style of the plots to seaborn 
     plt.style.use('seaborn')
@@ -43,28 +59,32 @@ def plot_losses(train_losses, valid_losses):
     # change the plot style to default
     plt.style.use('default')
 
-def validate(valid_loader, model, criterion, device, percentile):
+def validate(valid_loader, model, criterion, device, percentile, tensorboard_writer=None):
     '''
     Function for the validation step of the training loop
     '''
     activations = {}
     def save_activation(name, mod, inp, out):
-        activations[name] = out # don't detach or move to CPU here
+        if name not in activations.keys():
+            activations[name] = out
+        else:
+            activations[name] = torch.cat((activations[name],out))
 
     names = []
+    handles = []
     max_weights = []
     max_weights_percentile = []
     min_weights = []
     min_weights_percentile = []
     for name, module in model.named_modules():
         if isinstance(module, nn.Conv2d) or isinstance(module, nn.Linear):
-            module.register_forward_hook(partial(save_activation, name))
+            handles.append(module.register_forward_hook(partial(save_activation, name)))
             names.append(name)
-            max_weights.append(module.weight.max().cpu().numpy().round(2))
-            min_weights.append(module.weight.min().cpu().numpy().round(2))
+#             max_weights.append(module.weight.max().cpu().numpy().round(2))
+#             min_weights.append(module.weight.min().cpu().numpy().round(2))
             max_weights_percentile.append(np.percentile(module.weight.cpu().numpy(), percentile).round(2))
             min_weights_percentile.append(np.percentile(module.weight.cpu().numpy(), 100-percentile).round(2))
-            
+
     model.eval()
     running_loss = 0
     for X, y_true in valid_loader:
@@ -75,14 +95,21 @@ def validate(valid_loader, model, criterion, device, percentile):
         loss = criterion(y_hat, y_true)
         running_loss += loss.item() * X.size(0)
     epoch_loss = running_loss / len(valid_loader.dataset)
+    [handle.remove() for handle in handles] # remove forward hooks
     
-    str_output = ''.join(["{}: [{},{}]; ".format(names[i], str(min_weights[i]), str(max_weights[i])) for i in range(len(names))])
-    print("\t\tweights: " + str_output)
-    str_output = ''.join(["{}: [{},{}]; ".format(names[i], str(min_weights_percentile[i]), str(max_weights_percentile[i])) for i in range(len(names))])
-    print("\t" + str(percentile) + "% weights: " + str_output)
-    str_output = ''.join(["{}: {}, ".format(name, round(np.percentile(np.maximum(activation.cpu(),0), percentile),3)) for name, activation in activations.items()])
-    print("\t" + str(percentile) + "% activations: " + str_output)
-
+    if tensorboard_writer == None:
+#         str_output = ''.join(["{}: [{},{}]; ".format(names[i], str(min_weights[i]), str(max_weights[i])) for i in range(len(names))])
+#         print("\t\tweights: " + str_output)
+        str_output = ''.join(["{}: [{},{}]; ".format(names[i], str(min_weights_percentile[i]), str(max_weights_percentile[i])) for i in range(len(names))])
+        print("\t" + str(percentile) + "% weights: " + str_output)
+#         str_output = ''.join(["{}: {}, ".format(name, torch.quantile(torch.maximum(activation, torch.tensor(0)), percentile/100)) for name, activation in activations.items()])
+#         print("\t" + str(percentile) + "% pytorch activations: " + str_output)
+        str_output = ''.join(["{}: {}, ".format(name, round(np.percentile(np.maximum(activation.cpu(),0), percentile),3)) for name, activation in activations.items()])
+        print("\t" + str(percentile) + "% activations: " + str_output)
+    else:
+#         [tensorboard_writer.add_scalar(name, weights) for name, weights in zip(names, max_weights_percentile)]
+        [tensorboard_writer.add_histogram(name, activation) for name, activation in activations.items()]
+        
     return model, epoch_loss
 
 def get_weights_biases(model):
